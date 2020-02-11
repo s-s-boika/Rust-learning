0c650c4bfc99ab60747a0c3a4df45b67a4464041
Siarhei Boika <siarhei_boika@epam.com>
2020-02-11 21:38:51 +0300

    main.rs added loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,21 +9,24 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
 
-    let mut guess = String::new();
+        println!("Please input your guess.");
 
-    io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse()
-            .expect("Please type a number");
+        io::stdin().read_line(&mut guess)
+                .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse()
+                .expect("Please type a number");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win"),
+        }
     }
 }
