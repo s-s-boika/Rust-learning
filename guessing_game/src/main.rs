f74664fb22238d59b72b70a050ed2ceaf436af9d
Siarhei Boika <siarhei_boika@epam.com>
2020-02-11 21:48:57 +0300

    main.rs added continue and message if  input is not number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -18,8 +18,13 @@ fn main() {
         io::stdin().read_line(&mut guess)
                 .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-                .expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Wrong input. PLease type number!");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
 
