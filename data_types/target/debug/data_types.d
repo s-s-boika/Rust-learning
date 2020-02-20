8c9d968cec4b654fcca9b58cb326a46dc510d991
Siarhei Boika <siarhei_boika@epam.com>
2020-02-20 17:33:52 +0300

    new project Functions

---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/mnt/c/Git/Rust-learning/data_types/target/debug/data_types: /mnt/c/Git/Rust-learning/data_types/src/main.rs
---
+++ data_types/target/debug/deps/data_types-d31d2f6a7340e209.d
@@ -0,0 +1,5 @@
+/mnt/c/Git/Rust-learning/data_types/target/debug/deps/data_types-d31d2f6a7340e209: src/main.rs
+
+/mnt/c/Git/Rust-learning/data_types/target/debug/deps/data_types-d31d2f6a7340e209.d: src/main.rs
+
+src/main.rs:
